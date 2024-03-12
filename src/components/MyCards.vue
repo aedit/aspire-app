defc6a1d4989a4f317a9ae0441a016110a33a83d
Udit Sen <udisen97@gmail.com>
2024-03-12 07:41:34 +0530

    add transactions to cards

--- src/components/MyCards.vue
+++ src/components/MyCards.vue
@@ -51,7 +51,61 @@
     </div>
 
     <div class="col-xs-12 col-sm-12 col-md-6 col">
-        
+        <q-expansion-item class="q-mb-lg" expand-icon="icon-down-arrow" expand-icon-class="expansion-header-icon">
+          <template v-slot:header>
+            <div class="flex items-center text-secondary font-14 q-pa-lg">
+                <img src="@/assets/card-details.svg" alt="cancel" srcset="" class="q-mr-md">
+                <span>Card Details</span>
+            </div>
+          </template>
+          <div>
+            Card Details goes here
+          </div>
+        </q-expansion-item>
+
+        <q-expansion-item expand-icon="icon-down-arrow" expand-icon-class="expansion-header-icon">
+          <template v-slot:header>
+            <div class="flex items-center text-secondary font-14 q-pa-lg">
+                <img src="@/assets/transactions.svg" alt="cancel" srcset="" class="q-mr-md">
+                <span>Recent Transactions</span>
+            </div>
+          </template>
+          <div class="transactions-list">
+            <div v-if="getCardTransaction(slide).length === 0" class="flex flex-center text-negative font-14 q-pa-lg text-bold">
+              No transactions to show
+            </div>
+            <div v-else class="flex column">
+              <div v-for="item in getCardTransaction(slide)?.slice(0,4)" :key="item" 
+                class="transaction-list__item flex justify-between q-mx-lg q-py-md">
+                <div class="flex">
+                  <div class="flex q-mr-md">
+                  <q-icon :name="item.icon" class="rounded-borders q-pa-md" :style="{ backgroundColor: item.iconColor + '30', color: item.iconColor, borderRadius: '50%' }"></q-icon>
+                </div>
+
+                  <div class="flex column">
+                    <span class="text-semibold font-14 q-mb-sm">{{ item.transactionParty }}</span>
+                    <span class="font-13 q-mb-sm">{{ getTransactionDate(item.transactionDate) }}</span>
+                    <span class="text-accent font-12 text-semibold">
+                      <q-icon name="icon-Card" class="bg-accent q-pa-sm font-10" color="white" style="border-radius: 50%;"></q-icon>
+                      {{  item.transactionMessage }}</span>
+                    </div>
+                  </div>
+                <div>
+                  <span class="text-bold" :class="{'text-primary': item.transactionType === 'credit'}">
+                    {{ item.transactionType === 'credit' ? '+' : '-' }} S$
+                    {{  item.amount }}
+                  </span>
+                  <q-icon color="grey" name="icon-next" class="font-10 q-ml-md cursor-pointer" />
+                </div>
+            </div>
+            <div v-if="getCardTransaction(slide).length > 4" class="text-primary text-semibold flex q-py-md flex-center cursor-pointer" style="background-color: #EDFFF5;">
+              View all card transactions
+            </div>
+            </div>
+            
+            
+          </div> 
+        </q-expansion-item>
     </div>
   </div>
 </template>
@@ -69,12 +123,6 @@ export default defineComponent({
     },
     setup() {
         const store = useStore();
-
-        const cardMenuOptions = [
-            {
-                title: 'Freeze Limit'
-            }
-        ]
       
         const cards = computed(() => store.state.cards)
 
@@ -100,6 +148,13 @@ export default defineComponent({
             cards,
             getCardTransaction,
             freezeCard,
+        }
+    },
+    methods: {
+        getTransactionDate(date: Date) {
+            return date.toLocaleDateString('en-GB', {
+                day: '2-digit', month: 'short', year: 'numeric'
+            })
         }
     }
 });
@@ -135,6 +190,29 @@ export default defineComponent({
         span {
             text-align: center;
         }
+    }
+
+    .q-expansion-item {
+        border: 1px solid #F5F5F5;
+        box-shadow: 0 0 8px #0000000A;
+        border-radius: 8px;
+
+        .q-item {
+            padding: 0;
+            background-color: #F5F9FF;
+            justify-content: space-between;
+        }
+    }
+
+    .expansion-header-icon {
+        .icon-down-arrow {
+            font-size: 16px;
+            color: #D8E1F3;
+        }
+    }
+
+    .transaction-list__item {
+        border-bottom: 1px solid #F5F5F5;
     }
 }
 
