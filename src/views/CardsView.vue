097084ac0eeb2b1af1cbacdd00820b4b857b10c6
Udit Sen <udisen97@gmail.com>
2024-03-12 05:38:03 +0530

    added Cards page header

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -1,16 +1,56 @@
 <template>
-  <q-page class="flex flex-center">
-    Cards View
-    <img alt="Vue logo" src="../assets/logo.png">
+  <q-page class="cards-page flex column q-pa-xl">
+    <header class="q-mb-md">
+      <div class="q-pb-md font-14">
+        Available balance
+      </div>
+      <div class="flex justify-between flex-center">
+        <div class="flex flex-row flex-center">
+          <span class="font-13 text-bold bg-primary text-white flex flex-center q-mr-sm cards-page__header__currency">S$</span>
+          <span class="text-h5 text-bold">3,000</span>
+        </div>
+        
+        <q-btn color="accent" size="xs" no-caps class="font-13 flex flex-center q-pa-sm" @click="newCard = true">
+          <q-icon name="icon-add" />
+          <q-item-label class="q-ml-sm font-13">New card</q-item-label>
+        </q-btn>
+      </div>
+    </header>
+
+    <div>
+
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'CardsView',
   components: {
   },
+  setup() {
+    const newCard = ref(false)
+
+    return {
+      newCard
+    }
+  }
 });
 </script>
+
+<style lang="scss">
+.cards-page {
+
+  &__header {
+
+    &__currency {
+      border-radius: 4px;
+      padding: 2px 12px;
+    }
+
+  }
+
+}
+</style>
